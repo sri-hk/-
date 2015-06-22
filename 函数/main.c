9d318fcac095634c6ea4ea973c45be2b5d85ef65
Sri <sri@SrideMacBook-Pro.local>
2015-06-23 07:59:28 +0800

    Initial Commit

---
+++ 函数/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  函数
+//
+//  Created by Sri on 15/6/23.
+//  Copyright (c) 2015年 Sri. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
